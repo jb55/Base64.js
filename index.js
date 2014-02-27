c9820adcddeb32a4b019fe136f4a01c6b63782e1
William Casarin <will@casarin.me>
2014-02-27 15:11:50 -0800

    Attempt to fix odd issue

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var bind = require('bind');
-exports = module.exports;
+var exports = module.exports;
 exports.atob = exports.decode = bind(null, window.atob);
 exports.btoa = exports.encode = bind(null, window.btoa);
